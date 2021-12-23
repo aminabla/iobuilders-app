ddc4f45051c94951512923ddab0ad3f57211667a
agustinm <agustin.mina@eurobits.es>
2021-12-23 20:57:53 +0100

    one last general test to check that everything is wired as expected

---
+++ infrastructure/src/test/java/com/aminabla/wallet/infra/WalletAppTest.java
@@ -0,0 +1,14 @@
+package com.aminabla.wallet.infra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class WalletAppTest {
+
+    @Test
+    void loadApp(){
+    }
+}
